4a33ab92d85faa5246be792be3e6f679363dab30
dav1dvorobev <dav1dvorobev@mail.ru>
2026-05-20 12:46:02 +0300

    refactor(a3-transport): remove Display implementation

--- crates/a3-transport/src/message/message.rs
+++ crates/a3-transport/src/message/message.rs
@@ -6,13 +6,3 @@ pub struct Message {
     pub to: Address,
     pub body: String,
 }
-
-impl std::fmt::Display for Message {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{{\"from\":\"{}\",\"to\":\"{}\",\"body\":\"{}\"}}",
-            self.from, self.to, self.body
-        )
-    }
-}
